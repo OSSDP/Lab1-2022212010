dd2f342d4e2fb98d3a97083b7f80167781e6af3c
JohnFelix <644060892@qq.com>
2024-10-05 21:23:51 +0800

    1

--- les.java
+++ les.java
@@ -1,14 +1,11 @@
 package com.example.springboot.entity;
 
-
-
-
+//kkkkkkkkkkkkk
 
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @TableName("les")
 
@@ -23,7 +20,5 @@ public class les {
     private Integer lesmark;
     // 创建时间
     private String lesintroduction;
-
-
 
 }
